d00a6854abb0de6f0154d957c04604507cd96437
Mohammad222PR <mohammades13851@gmail.com>
2024-03-24 14:14:27 +0330

    password_change_with_email

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    # changing password urls
     path('password/reset/', views.PasswordResetView.as_view(), name='password_reset'),
     path('password/reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password/reset/confirm/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(),
