fea286f97bcca105104ea9d6ace7ac7a52f8826e
Mohammad222PR <mohammades13851@gmail.com>
2024-03-30 02:00:04 +0330

    x

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from cart.models import Cart, CartItem
+
+
 # Register your models here.
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_date', 'updated_date']
+    list_filter = ['created_date', 'updated_date']
+    search_fields = ['user']
+    ordering = ['user', 'created_date', 'updated_date']
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['cart', 'product', 'quantity', 'created_date', 'updated_date']
+    list_filter = ['created_date', 'updated_date']
+    search_fields = ['cart']
+    ordering = ['cart', 'product', 'quantity', 'created_date', 'updated_date']
